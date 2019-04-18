3d8234a02c252ed86f84edf547d797934a899ef3
CocoThePimp <jonathan.lutz@gmx.fr>
2019-04-18 14:31:55 +0200

    first

---
+++ README.md
@@ -0,0 +1,10 @@
+
+
+I just made a little game to improve my js skills. 
+
+
+My purpose is to make a street fighter like with the "rock paper scissors" game.
+
+
+https://cocothepimp.github.io/Rock-Paper-Scissors/
+
---
+++ script.js
@@ -0,0 +1,111 @@
+
+var items = ["rock","paper","scissors"]
+var playerSelect
+var playerScored = 0
+var computerSelect
+var computerScored = 0
+
+
+function computerChoice () {
+computerSelect = items[Math.floor(Math.random()*items.length)]
+}
+
+
+document.getElementById("rock").addEventListener("click", function (){ playerSelect = "rock" ; launchGame() })
+document.getElementById("paper").addEventListener("click", function (){ playerSelect = "paper" ; launchGame() })
+document.getElementById("scissors").addEventListener("click", function (){ playerSelect = "scissors" ; launchGame() })
+
+
+function playground () {
+  console.log(computerSelect)
+  console.log(playerSelect)
+    if (playerSelect === "scissors" && computerSelect === "paper"){
+      playerSelect = null ;
+      playerScored++ 
+
+      return "Player scored";
+    }
+    else if (playerSelect === "rock" && computerSelect === "scissors"){
+      playerSelect = null ; 
+      playerScored++ 
+      return "Player scored";
+    }
+    else if (playerSelect === "paper" && computerSelect === "rock"){
+      playerSelect = null ;
+      playerScored++  
+      return "Player scored";
+    }
+    else if  (playerSelect == computerSelect) {
+      playerSelect = null ; 
+      return "Tie Game";
+    }
+    else {
+      playerSelect = null ;
+      computerScored++  
+      return "IA scored";
+    }
+};
+
+
+function displayResult (){
+  document.getElementById("result").innerHTML = playground();
+  document.getElementById("result2").innerHTML = "Score : " + playerScored + "-" + computerScored;
+}
+
+
+function score() {
+  if (computerScored === 5){
+    document.getElementById("result3").innerHTML = "IA WINS !";
+    playerScored = 0
+    computerScored = 0
+  };
+
+  if (playerScored === 5){
+    document.getElementById("result3").innerHTML = "YOU WINS !";
+    playerScored = 0
+    computerScored = 0
+  };
+}
+
+
+function launchGame() {
+  computerChoice () ; 
+  displayResult () ;
+  score ();
+}
+
+
+// // Annimation
+// var tID; //we will use this variable to clear the setInterval()
+
+// function stopAnimate() {
+// clearInterval(tID);
+// } //end of stopAnimate()
+
+// function animateScript() {
+//   var position = 0; //start position for the image slicer
+//   const interval = 300; //100 ms of interval for the setInterval()
+//   const diff = 102.4;     //diff as a variable for position offset
+//   tID = setInterval ( () => {
+//   document.getElementById("image").style.backgroundPosition = 
+//   `-${position}px 0px`; 
+// //we use the ES6 template literal to insert the variable "position"
+//   if (position < 1024)
+//   { position = position + diff;}
+// //we increment the position by 256 each time
+//   else
+//   { position = 0; }
+// //reset the position to 256px, once position exceeds 1536px
+// }
+//   , interval ); //end of setInterval
+// } //end of animateScript()
+
+
+// LIFE BARRE
+
+
+
+
+
+
+
